06150222d70ed2d37d9c49a16039ac46bbea7e17
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2023-06-24 09:42:27 +0100

    dep. and intents

--- constants.js
+++ constants.js
@@ -3,7 +3,12 @@ const { Client, GatewayIntentBits, Collection } = require("discord.js");
 module.exports =
 {
     // Discord Client representing the Bot/App
-    DiscordClient: new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildIntegrations ] }),
+    DiscordClient: new Client({ 
+        intents: [ 
+            GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildIntegrations,
+            GatewayIntentBits.GuildMembers
+        ]
+    }),
 
     // Collections that are used in many locations
     Collections: {
