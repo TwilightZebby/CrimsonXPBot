ba21b258fa3a26b3ad84b5d6a7812cac2e045e77
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2023-08-15 12:56:02 +0100

    Update Broadcasts.js

--- BotModules/Levelling/Broadcasts.js
+++ BotModules/Levelling/Broadcasts.js
@@ -48,6 +48,55 @@ module.exports = {
         }
 
         return;
+    },
+
+
+
+
+
+    /**
+     * Broadcasts the Level DOWN Message, if enabled in Guild
+     * @param {Message} message
+     * @param {Number} level
+     */
+    async levelDown(message, level)
+    {
+        // Check Guild has enabled Broadcast Messages
+        if ( await GuildConfig.exists({ guildId: message.guildId }) == null ) { return; }
+
+        let fetchedConfig = await GuildConfig.findOne({ guildId: message.guildId });
+
+        if ( fetchedConfig.broadcastChannel === "DISABLE" ) { return; }
+        
+        
+        // Contruct string
+        let levelDownMessage = fetchedConfig.rankdownMessage.replace("{user}", `<@${message.author.id}>`).replace("{level}", `${level}`);
+        
+        // Set mention state
+        let enableMentions = false;
+        let fetchedUserConfig = null;
+
+        if ( await UserConfig.exists({ userId: message.author.id }) == null ) { /* Do nothing */ }
+        fetchedUserConfig = await UserConfig.findOne({ userId: message.author.id });
+        enableMentions = fetchedUserConfig.broadcastMentions;
+
+        
+        if ( fetchedConfig.broadcastChannel === "CURRENT" )
+        {
+            // Broadcast in current Channel
+            if ( enableMentions ) { await message.channel.send({ allowedMentions: { parse: ['users'] }, content: levelDownMessage }); }
+            else { await message.channel.send({ allowedMentions: { parse: [] }, content: levelDownMessage }); }
+        }
+        else
+        {
+            // Broadcast in specified Channel
+            let guildChannel = await message.guild.channels.fetch(fetchedConfig.broadcastChannel);
+            
+            if ( enableMentions ) { await guildChannel.send({ allowedMentions: { parse: ['users'] }, content: levelDownMessage }); }
+            else { await guildChannel.send({ allowedMentions: { parse: [] }, content: levelDownMessage }); }
+        }
+
+        return;
     }
 
 };
